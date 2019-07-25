b92639c84aeeaf881db2de6748c619ff88727eab
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-25 15:19:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,7 @@
 class CashRegister
-  attr_reader :total
-  def initialize {discount =20
+  attr_reader :total, :discount
+  attr_accessor :total
+  def initialize (discount =20)
     @total = 0
     @discount = discount
   end
